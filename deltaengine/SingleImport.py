215ed55814719431e80c578790b5d0fb847e3788
Jacob Sansbury <me@workbyjacob.com>
2018-12-01 14:14:30 -0500

    dev experience changes

---
+++ deltaengine/SingleImport.py
@@ -0,0 +1,24 @@
+import deltaengine.Reports as Reports
+import deltaengine as engine
+
+import plotly
+import plotly.plotly as plt
+import plotly.graph_objs as go
+
+import pandas as pd
+import numpy as np
+
+import warnings
+
+
+warnings.filterwarnings("ignore")
+plotly.tools.set_credentials_file(username='sansburyjacob', api_key='aRJN1U0jX9f7px1HKdd2')
+
+report = Reports
+
+plt = plt
+go = go
+pd = pd
+np = np
+
+engine = engine
--- test.py
+++ test.py
@@ -1,3 +1,1 @@
-from deltaengine.Reports import ReportData, ReportTypes
-
-print(ReportData(report_type=ReportTypes.THREE_MONTHS, props=["parker-visits-in-spring-2019"]).data_set.get_windfalls())
+from deltaengine.SingleImport import plt, report, engine, tool
