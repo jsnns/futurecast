a73a48e2eb4d1a4d4761254147ac0eff661a1735
Jacob Sansbury <sansburyjacob@gmail.com>
2019-01-30 21:31:49 -0500

    daily updates

--- data/reality.py
+++ data/reality.py
@@ -25,18 +25,18 @@ EverySunday = Schedule(start=datetime(2019,1,20), interval=relativedelta(weeks=1
 RyanKautz = Schedule(start=datetime(2019,1,18), interval=relativedelta(months=1), end=datetime(2019,8,1))
 
 accounts = [
-    Account(name="360 Checking", balance=539),
-    Account(name="360 Savings", balance=5642),
+    Account(name="360 Checking", balance=6.46),
+    Account(name="360 Savings", balance=5232),
 ]
 
 transactions = [
     # Income
     Transaction(name="Bridgewater",     category="income",      schedule=Bridgewater,       value=2650,     monthly=5300),
-    Transaction(name="ChickFilA",       category="income",      schedule=ChickFilA,         value=500,      monthly=1000),
+    Transaction(name="ChickFilA",       category="income",      schedule=ChickFilA,         value=200,      monthly=800),
     
     # Expenses
     Transaction(name="Ryan",            category="debt",         schedule=RyanKautz,        value=-1750),
-    Transaction(name="1KennedyFlats",   category="rent",         schedule=MonthlyOn(1),     value=-1750),
+    Transaction(name="1KennedyFlats",   category="rent",         schedule=MonthlyOn(1),    value=-1750),
     Transaction(name="Sprint",          category="bills",        schedule=MonthlyOn(10),    value=-435),
     Transaction(name="Comcast",         category="bills",        schedule=MonthlyOn(17),    value=-190),
     Transaction(name="Eversource",      category="bills",        schedule=MonthlyOn(4),     value=-219),
@@ -48,7 +48,8 @@ transactions = [
     Transaction(name="Fair",            category="once",        schedule=Once(2019,2,1),    value=-545),
     Transaction(name="HalfFair",        category="once",        schedule=Once(2019,2,15),   value=-280),
     Transaction(name="TowCar",          category="once",        schedule=Once(2019,2,15),   value=-200),
-    Transaction(name="Mechanic",        category="once",        schedule=Once(2019,2,10),   value=-1875)
+    Transaction(name="Mechanic",        category="once",        schedule=Once(2019,2,10),   value=-1875),
+    Transaction(name="SkyFlix@",        category="once",        schedule=Once(2019,2,4),    value=500),
 ]
 
 def make_html(tx_set, bal):
@@ -73,9 +74,9 @@ def make_html(tx_set, bal):
         <img src="///Users/jacob/personal/adder/output/{datetime.today().strftime("%Y-%d-%m")}.png" />
         <img src="///Users/jacob/personal/adder/output/budget-{datetime.today().strftime("%Y-%d-%m")}.png"/>
     
-        {tabulate(budget["budget"], headers=("Category", "$", "% Budget"), tablefmt=OUTPUT_FOMAT)}
+        {tabulate(budget["budget"], headers=("Category", "$", "% Budget"), tablefmt="html")}
         <br>
-        {tabulate(bal.stats, headers=("Stat", "Value"), tablefmt=OUTPUT_FOMAT)}
+        {tabulate(bal.stats, headers=("Stat", "Value"), tablefmt="html")}
     </div>
     """
 
@@ -85,7 +86,6 @@ if __name__ == "__main__":
 
     REPORT_LEGTH_MONTHS = 6
     SHOW_ROWS = 15
-    OUTPUT_FOMAT = "html"
     
     tx_set = TransactionSet(transactions=transactions, end=datetime.today() + relativedelta(months=REPORT_LEGTH_MONTHS))
     bal = BalanceSheet(log=tx_set.log, accounts=accounts)
@@ -93,6 +93,7 @@ if __name__ == "__main__":
     tx_set.budget_plot()
     bal.create_plot()
 
-    if OUTPUT_FOMAT == "html":
-        with open(f"output/html/{datetime.today().strftime('%Y-%d-%m')}.html", "w") as f:
-            f.write(make_html(tx_set, bal))
+    print(tabulate(bal.stats, headers=("Stat", "Value")))
+
+    with open(f"output/html/{datetime.today().strftime('%Y-%d-%m')}.html", "w") as f:
+        f.write(make_html(tx_set, bal))
