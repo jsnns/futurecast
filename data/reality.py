00320053d9451993db31e10892ef77908d0eefb4
Jacob Sansbury <sansburyjacob@gmail.com>
2019-02-04 19:24:35 -0500

    Daily Changes: Feb 4, 2019
    Summary: Sold BRZ tires and got paid for SkyFlix work. It feels awesome.
    
    Wisdom: The small expenses add up, saving every penny counts.

--- data/reality.py
+++ data/reality.py
@@ -26,22 +26,21 @@ EverySunday = Schedule(start=datetime(2019,1,20), interval=relativedelta(weeks=1
 RyanKautz = Schedule(start=datetime(2019,1,18), interval=relativedelta(months=1), end=datetime(2019,8,1))
 
 accounts = [
-    Account(name="360 Checking",    balance=677.86),
-    Account(name="360 Savings",     balance=5034.07 + 495),
+    Account(name="360 Checking",    balance=572.47),
+    Account(name="360 Savings",     balance=5529.07),
 ]
 
 transactions = [
     # Income
     Transaction(name="Bridgewater",     category="income",      schedule=Bridgewater,       value=2650,     monthly=5300),
-    Transaction(name="ChickFilA",       category="income",      schedule=ChickFilA,         value=200,      monthly=800),
+    Transaction(name="ChickFilA",       category="income",      schedule=ChickFilA,         value=200,      monthly=400),
     
     # Expenses
     Transaction(name="Ryan",            category="debt",         schedule=RyanKautz,        value=-1750),
-    Transaction(name="1KennedyFlats",   category="rent",         schedule=MonthlyOn(2),     value=-1850,    autopay=True),
+    Transaction(name="1KennedyFlats",   category="rent",         schedule=MonthlyOn(2),     value=-1800,    autopay=True),
     Transaction(name="Sprint",          category="bills",        schedule=MonthlyOn(10),    value=-435,     autopay=True),
     Transaction(name="Comcast",         category="bills",        schedule=MonthlyOn(17),    value=-190),
     Transaction(name="Eversource",      category="bills",        schedule=MonthlyOn(4),     value=-100,     autopay=True),
-    Transaction(name="PrimeStorage",    category="storage",      schedule=MonthlyOn(30),    value=-89,      autopay=True),
     Transaction(name="Transport",       category="transport",    schedule=EverySunday,      value=-132,     monthly=-528),
     Transaction(name="Food",            category="food",         schedule=EverySunday,      value=-100,     monthly=-400),
     
@@ -50,7 +49,8 @@ transactions = [
     Transaction(name="HalfFair",        category="once",        schedule=Once(2019,2,15),   value=-280),
     Transaction(name="TowCar",          category="once",        schedule=Once(2019,2,15),   value=-200),
     Transaction(name="Mechanic",        category="once",        schedule=Once(2019,2,10),   value=-1875),
-    Transaction(name="SkyFlix@",        category="once",        schedule=Once(2019,2,4),    value=500),
+    Transaction(name="SkyFlix@",        category="once",        schedule=Once(2019,2,6),    value=838),
+    Transaction(name="SellTires",       category="once",        schedule=Once(2019,2,10),   value=650),
     Transaction(name="CTTaxReturn",     category="once",        schedule=Once(2019,2,14),   value=222),
 ]
 
@@ -67,5 +67,5 @@ if __name__ == "__main__":
 
     print(tabulate(bal.stats, headers=("Stat", "Value")))
 
-    with open(f"output/html/{datetime.today().strftime('%Y-%d-%m')}.html", "w") as f:
+    with open(f"output/html/{datetime.today().strftime('%Y-%m-%d')}.html", "w") as f:
         f.write(make_html(tx_set, bal))
