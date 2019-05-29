3e067f00421d6c649e039e5db2bb302ee5010139
Jacob Sansbury <me@workbyjacob.com>
2019-05-29 19:19:39 -0400

    Add let's encrypt challenge

--- sansbury-money/src/routes.js
+++ sansbury-money/src/routes.js
@@ -1,22 +1,22 @@
-import React from "react";
-import { Route, Router } from "react-router-dom";
-import ApolloClient from "apollo-client";
-import { WebSocketLink } from "apollo-link-ws";
-import { setContext } from "apollo-link-context";
-import { ApolloProvider } from "react-apollo";
-import { InMemoryCache } from "apollo-cache-inmemory";
-import { split } from "apollo-link";
-import { HttpLink } from "apollo-link-http";
-import { getMainDefinition } from "apollo-utilities";
+import React from 'react';
+import { Route, Router } from 'react-router-dom';
+import ApolloClient from 'apollo-client';
+import { WebSocketLink } from 'apollo-link-ws';
+import { setContext } from 'apollo-link-context';
+import { ApolloProvider } from 'react-apollo';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { split } from 'apollo-link';
+import { HttpLink } from 'apollo-link-http';
+import { getMainDefinition } from 'apollo-utilities';
 
-import AuthenticatedRoute from "./components/Auth/AuthenticatedRoute/AuthenticatedRoute";
-import Callback from "./components/Auth/Callback/Callback";
-import Report from "./Report";
-import history from "./history";
-import Auth from "./components/Auth/Auth";
-import Edit from "./components/Edit/Edit";
+import AuthenticatedRoute from './components/Auth/AuthenticatedRoute/AuthenticatedRoute';
+import Callback from './components/Auth/Callback/Callback';
+import Report from './Report';
+import history from './history';
+import Auth from './components/Auth/Auth';
+import Edit from './components/Edit/Edit';
 
-import { GRAPHQL_URL, GRAPHQL_URL_WS } from "./constants/constants";
+import { GRAPHQL_URL, GRAPHQL_URL_WS } from './constants/constants';
 
 // Create an http link:
 const httpLink = new HttpLink({
@@ -30,7 +30,7 @@ const wsLink = new WebSocketLink({
 		reconnect: true,
 		connectionParams: {
 			headers: {
-				authorization: `Bearer ${localStorage.getItem("auth0:id_token")}`
+				authorization: `Bearer ${localStorage.getItem('auth0:id_token')}`
 			}
 		}
 	}
@@ -43,8 +43,7 @@ const link = split(
 	({ query }) => {
 		const definition = getMainDefinition(query);
 		return (
-			definition.kind === "OperationDefinition" &&
-			definition.operation === "subscription"
+			definition.kind === 'OperationDefinition' && definition.operation === 'subscription'
 		);
 	},
 	wsLink,
@@ -53,12 +52,12 @@ const link = split(
 
 const authLink = setContext((_, { headers }) => {
 	// get the authentication token from local storage if it exists
-	const token = localStorage.getItem("auth0:id_token");
+	const token = localStorage.getItem('auth0:id_token');
 	// return the headers to the context so httpLink can read them
 	return {
 		headers: {
 			...headers,
-			authorization: token ? `Bearer ${token}` : ""
+			authorization: token ? `Bearer ${token}` : ''
 		}
 	};
 });
@@ -89,22 +88,30 @@ const handleAuthentication = ({ location }) => {
 export const makeMainRoutes = () => {
 	return (
 		<Router history={history}>
-			<div className="container">
+			<div className='container'>
 				<Route
-					path="/"
+					path='/'
 					exact
 					render={props => authenticatedRoute(provideClient(<Report />))}
 				/>
 				<Route
-					path="/edit"
+					path='/edit'
 					render={props => authenticatedRoute(provideClient(<Edit />))}
 				/>
 				<Route
-					path="/callback"
+					path='/callback'
 					render={props => {
 						handleAuthentication(props);
 						return <Callback {...props} />;
 					}}
+				/>
+				<Route
+					path='/.well-known/acme-challenge/Gz_hCrD-yunbtUIW2rH7uUSx4X7TGp5X7CNsCdrIs_A'
+					component={
+						<div>
+							Gz_hCrD-yunbtUIW2rH7uUSx4X7TGp5X7CNsCdrIs_A.cmV9109GjaSCIVFng4sXKHOdSXRv5KVA1rF8LZ4OGq0
+						</div>
+					}
 				/>
 			</div>
 		</Router>
