1132df011bcee6d83ae3d11c53c28d910ec5a4d4
Jacob Sansbury <me@workbyjacob.com>
2019-05-30 12:56:20 -0400

    Update auth callback variables

--- src/components/Auth/auth0-variables.js
+++ src/components/Auth/auth0-variables.js
@@ -4,7 +4,7 @@ export const AUTH_CONFIG = {
 	domain: AUTH0_AUTH_DOMAIN,
 	clientId: AUTH0_CLIENT_ID,
 	callbackUrl:
-		process.env.NODE_ENV === "dev"
+		process.env.NODE_ENV === "development"
 			? "http://localhost:3000/callback"
 			: "https://futurecast.sansburyhome.com/callback"
 };
