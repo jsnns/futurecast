556d49dd3bf150c5181ac33daaab117bf8982e25
Jacob Sansbury <me@workbyjacob.com>
2019-05-30 11:44:22 -0400

    updateci

--- src/components/Auth/auth0-variables.js
+++ src/components/Auth/auth0-variables.js
@@ -3,5 +3,8 @@ import { AUTH0_AUTH_DOMAIN, AUTH0_CLIENT_ID } from "../../constants";
 export const AUTH_CONFIG = {
 	domain: AUTH0_AUTH_DOMAIN,
 	clientId: AUTH0_CLIENT_ID,
-	callbackUrl: process.env.NODE_ENV === 'dev' ? "http://localhost:3000/callback" : "https://futurecast.sansburyhome.com"
+	callbackUrl:
+		process.env.NODE_ENV === "dev"
+			? "http://localhost:3000/callback"
+			: "https://futurecast.sansburyhome.com"
 };
