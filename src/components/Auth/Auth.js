5cf98ccf6cddae7951d203a458678727c660a608
Jacob Sansbury <me@workbyjacob.com>
2019-06-25 17:45:02 -0400

    remove static from methods

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -77,7 +77,7 @@ export default class Auth {
     window.location.replace("/");
   };
 
-  static logout = () => {
+  logout = () => {
     // Clear access token and ID token from local storage
     localStorage.removeItem("auth0:access_token");
     localStorage.removeItem("auth0:id_token");
@@ -87,7 +87,7 @@ export default class Auth {
     history.replace("/");
   };
 
-  static isAuthenticated = () => {
+  isAuthenticated = () => {
     // Check whether the current time is past the
     // access token's expiry time
     let expiresAt = JSON.parse(localStorage.getItem("auth0:expires_at"));
