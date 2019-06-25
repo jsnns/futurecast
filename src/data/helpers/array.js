85706481ec70279f83c652bac23800d3eb64695b
Jacob Sansbury <me@workbyjacob.com>
2019-06-25 17:35:34 -0400

    remove decending

--- src/data/helpers/array.js
+++ src/data/helpers/array.js
@@ -5,12 +5,6 @@ export const cmp = {
     if (a > b) return 1;
     else if (b > a) return -1;
     return 0;
-  },
-
-  descending: (a, b) => {
-    if (a > b) return -1;
-    else if (b > a) return 1;
-    return 0;
   }
 };
 
@@ -19,21 +13,12 @@ export const math = {};
 export const sortAscendingByKey = (array, key) =>
   array.sort((a, b) => cmp.ascending(a[key], b[key]));
 
-export const sortDecendingByKey = (array, key) =>
-  array.sort((a, b) => cmp.descending(a[key], b[key]));
-
 export const sortAscending = array => array.sort(cmp.ascending);
 
-export const sortDecending = array => array.sort(cmp.descending);
-
 export const getMinimum = array => sortAscending(array)[0];
-
-export const getMaximum = array => sortDecending(array)[0];
 
 export const getKey = (array, key) => array.map(a => a[key]);
 
 export const sumArray = array => array.reduce(add, 0);
-
-export const getMean = array => sumArray(array) / array.length;
 
 export const add = (a, b) => a + b;
