b1d22354f4d7ffb5c2739d5e933b20c5bee6eeb2
Jacob Sansbury <me@workbyjacob.com>
2019-06-25 16:08:59 -0400

    create single edit files

--- src/components/Edit/Edit.js
+++ src/components/Edit/Edit.js
@@ -2,12 +2,63 @@ import React, { Component } from "react";
 
 import { Box } from "grommet";
 
-import EditTransactions from "./Transactions/Transactions";
-import EditAccounts from "./Accounts/Accounts";
 import DashboardWidget from "../shared/DashboardWidget";
-import Subscriptions from "./Subscriptions/Subscriptions";
+import EditGraphql from "./EditGraphql";
+import gql from "graphql-tag";
 import Header from "../shared/Header";
-import Wishes from "./Wishlist/Wishlist";
+
+const TRANSACTION_SUB = gql`
+    subscription {
+        transactions {
+            id
+            value
+            name
+            start
+            end
+            category
+            interval_days
+            interval_months
+        }
+    }
+`;
+
+const SUBSCRIPTIONS_SUB = gql`
+    subscription {
+        subscriptions {
+            id
+            name
+            price
+            category
+            yearly
+            owner
+            usefulness
+        }
+    }
+`;
+
+const ACCOUNTS_SUB = gql`
+    subscription {
+        accounts {
+            id
+            name
+            balance
+        }
+    }
+`;
+
+const WISHES_SUB = gql`
+    subscription {
+        wishes {
+            id
+            name
+            price
+            category
+            owner
+            usefulness
+        }
+    }
+`;
+
 
 
 class Edit extends Component {
@@ -50,21 +101,154 @@ class Edit extends Component {
 
     return (
       <Box>
-        <Header></Header>
+        <Header />
         <Box direction="row-responsive">
           <DashboardWidget title="Transactions" basis="1/2">
-            <EditTransactions />
+            <EditGraphql
+              subscription={TRANSACTION_SUB}
+              table="transactions"
+              columns={[
+                {
+                  header: "Name",
+                  property: "name"
+                },
+                {
+                  header: "Value",
+                  property: "value"
+                },
+                {
+                  header: "Category",
+                  property: "category"
+                }
+              ]}
+              fields={[
+                {
+                  name: "Name",
+                  property: "name"
+                },
+                {
+                  name: "Value",
+                  property: "value"
+                },
+                {
+                  name: "Category",
+                  property: "category"
+                },
+                {
+                  name: "Start Date",
+                  property: "start"
+                },
+                {
+                  name: "End Date",
+                  property: "end"
+                },
+                {
+                  name: "Interval Months",
+                  property: "interval_months"
+                },
+                {
+                  name: "Interval Days",
+                  property: "interval_days"
+                }
+              ]}/>
           </DashboardWidget>
           <DashboardWidget title="Accounts" basis="1/2">
-            <EditAccounts />
+            <EditGraphql
+              subscription={ACCOUNTS_SUB}
+              table="accounts"
+              columns={[
+                {
+                  header: "Name",
+                  property: "name"
+                },
+                {
+                  header: "Balance",
+                  property: "balance"
+                }
+              ]}
+              fields={[
+                {
+                  name: "Balance",
+                  property: "balance"
+                }
+              ]}
+            />
           </DashboardWidget>
         </Box>
         <Box direction="row-responsive">
           <DashboardWidget title="Subscriptions" basis={"1/2"}>
-            <Subscriptions />
+            <EditGraphql
+              subscription={SUBSCRIPTIONS_SUB}
+              table="subscriptions"
+              columns={[
+                {
+                  header: "Name",
+                  property: "name"
+                },
+                {
+                  header: "Price",
+                  property: "price"
+                },
+                {
+                  header: "Category",
+                  property: "category"
+                },
+                {
+                  header: "Usefulness",
+                  property: "usefulness"
+                }]}
+              fields={[
+                {
+                  name: "Price",
+                  property: "price"
+                },
+                {
+                  name: "Category",
+                  property: "category"
+                },
+                {
+                  name: "Usefulness",
+                  property: "usefulness"
+                }
+              ]}
+            />
           </DashboardWidget>
-          <DashboardWidget title="wishes" basis={"1/2"}>
-            <Wishes />
+          <DashboardWidget title="Wishes" basis={"1/2"}>
+            <EditGraphql
+              subscription={WISHES_SUB}
+              table="wishes"
+              columns={[
+                {
+                  header: "Name",
+                  property: "name"
+                },
+                {
+                  header: "Price",
+                  property: "price"
+                },
+                {
+                  header: "Category",
+                  property: "category"
+                },
+                {
+                  header: "Usefulness",
+                  property: "usefulness"
+                }]}
+              fields={[
+                {
+                  name: "Price",
+                  property: "price"
+                },
+                {
+                  name: "Category",
+                  property: "category"
+                },
+                {
+                  name: "Usefulness",
+                  property: "usefulness"
+                }
+              ]}
+            />
           </DashboardWidget>
         </Box>
       </Box>
